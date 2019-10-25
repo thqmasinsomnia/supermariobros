c0b48fb936c93c48deccff133ddebf9e4bee59dc
vincentdeaugustine <vinnied44@csu.fullerton.edu>
2019-10-25 10:58:58 -0700

    First branch commit

---
+++ pipe.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.sprite import Sprite
+from pygame.sprite import Group
+
+class Pipe(Sprite):
+    def __init__(self):
+
+    # Draw a rectangle around the pipe
+    # Rectangle is invisible
+    # We can land on the rect
+    # When running into pipe, you can't move past it
+    # If pipe can be entered
+        # If pressed down
+            # Enter pipe, change level
+
+    # In the main file, we will create a list of pipes when the
+    # Pipes will
